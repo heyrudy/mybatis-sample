a55f56664198a9629e9a4b0947d45df4815e615d
Kokou KOUMAGNA <kokou.koumagnagottoh@gmail.com>
2020-10-03 22:04:26 +0200

    :recycle: the code

--- src/main/java/com/heyrudy/mybatissample/configuration/AppConfiguration.java
+++ src/main/java/com/heyrudy/mybatissample/configuration/AppConfiguration.java
@@ -8,30 +8,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfiguration {
 
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSource dataSource() {
-//        return DataSourceBuilder.create().build();
-//    }
-//
-//    @Bean
-//    public SqlSessionFactory sqlSessionFactory() throws Exception {
-//        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-//        factoryBean.setDataSource(dataSource());
-//        return factoryBean.getObject();
-//    }
-//
-//    @Bean
-//    public SqlSessionTemplate sqlSession() throws Exception {
-//        return new SqlSessionTemplate(sqlSessionFactory());
-//    }
-//
-//    @Bean
-//    public CityRepository cityRepository() throws Exception {
-//        try (SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory())) {
-//            return sqlSessionTemplate.getMapper(CityRepository.class);
-//        }
-//    }
-
     @Bean
     public CityMapper mapper() {
         return new CityMapperImpl();
