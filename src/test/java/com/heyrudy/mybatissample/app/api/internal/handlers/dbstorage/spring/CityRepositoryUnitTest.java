f0bf01cabb74e65fdd2c833bc511b922709a755f
heyrudy <kokou.koumagnagottoh@gmail.com>
2023-01-08 23:27:31 +0100

    :white_check_mark: update repository unit test of saving a city into a datastore

--- src/test/java/com/heyrudy/mybatissample/app/api/internal/handlers/dbstorage/spring/CityRepositoryUnitTest.java
+++ src/test/java/com/heyrudy/mybatissample/app/api/internal/handlers/dbstorage/spring/CityRepositoryUnitTest.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,7 +31,7 @@ class CityRepositoryUnitTest {
                 .country("France")
                 .state("Paris75")
                 .build();
-        doNothing().when(repository).save(expected);
+        when(repository.save(expected)).thenReturn(expected);
         // Act
         repository.save(expected);
         // Assert
